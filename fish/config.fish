06b2ff997861ea019cf22f8d21f61c54bfc44cdd
Sebastian Jastrzebski <sebby2k@gmail.com>
2020-08-12 08:57:36 -0400

    fish: change term

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ set -gx PATH $PATH ~/.local/bin ~/.cargo/bin ~/sdk/flutter/bin
 set -gx ANDROID_HOME ~/sdk/android
 set -gx DFS_HOST_INT 192.168.1.11
 set -gx FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(code {})+abort'"
-set -gx TERM xterm-color
+set -gx TERM xterm-256color
 
 # cli
 alias cat='bat'
