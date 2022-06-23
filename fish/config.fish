3b98acf29fb0db40592a4d952b15b2ebf0cc2d7f
Sebastian Jastrzebski <sebby2k@gmail.com>
2022-06-23 17:34:31 -0400

    fish: add new aliases

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,11 @@ set -gx FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(code {})+abort'"
 
 # aliases
 alias cat='bat'
+alias cr='org.chromium.Chromium -enable-features=UseOzonePlatform -ozone-platform=wayland'
 alias code='com.visualstudio.code'
 alias help='tldr'
 alias jn='journalctl'
 alias pv="fzf --preview 'bat --color \"always\" {}'"
 alias sd='systemctl'
-alias pac-all="pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse"
-alias pac-installed="pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
+alias pacall="pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse"
+alias paci="pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
