09eda9669cea5a5e988d3ba58bf918b4288cd88e
Sebastian Jastrzebski <sebby2k@gmail.com>
2022-09-13 10:18:40 -0400

    lf: update bindings

--- lf/pv.sh
+++ lf/pv.sh
@@ -5,5 +5,7 @@ case "$1" in
     *.pdf) pdftotext "$1" -;;
     *.tar*) tar tf "$1";;
     *.zip) unzip -l "$1";;
+    *.jpg|*.jpeg|*.png) mediainfo "$1";;
+    *.m4v|*.mkv|*.mov|*.mp4) mediainfo "$1";;
     *) bat "$1";;
 esac
